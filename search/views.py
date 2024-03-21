0f3641744cc256e6806841df473677e448845c42
Tejasram <tejasram03@gmail.com>
2024-03-21 21:23:53 +0530

    Update views.py for waiting page

--- search/views.py
+++ search/views.py
@@ -36,30 +36,37 @@ def get_pdf_text(pdf):
     return text
 
 
-def results(request):
-    job_type = request.POST.get('job_type')
-    work_preference = request.POST.get('work_preference')
-    resume = request.FILES['resume']
-    document = FilesUpload.objects.create(file = resume)
-    document.save()
-    
-    
-    resume_text = get_pdf_text(f"/home/tejasram/CS_projects0/LlaMaHunt/llamahunt/media/{resume.name}")
-    # print(resume_text)
-    additional_info = request.POST.get('additional_info')
-    data = {'job_type':job_type,
-        'work_preference':work_preference,
-        'additional_info':additional_info
-    }
-    print(data)
-    # print(**data)
-    
-    # job_application_instance = JobApplication(**data)
-    
-    # print(job_application_instance)
-    
-    job_application_instance = get_results(resume_content=resume_text, preferences=data)
+def results(request):   
 
-    # print(job_application_instance)
+    results_parsed = False
+
+    if results_parsed:
+
+        job_type = request.POST.get('job_type')
+        work_preference = request.POST.get('work_preference')
+        resume = request.FILES['resume']
+        document = FilesUpload.objects.create(file = resume)
+        document.save()
+        
+        
+        resume_text = get_pdf_text(f"/home/tejasram/CS_projects0/LlaMaHunt/llamahunt/media/{resume.name}")
+        # print(resume_text)
+        additional_info = request.POST.get('additional_info')
+        data = {'job_type':job_type,
+            'work_preference':work_preference,
+            'additional_info':additional_info
+        }
+        print(data)
+        # print(**data)
+        
+        # job_application_instance = JobApplication(**data)
+        
+        # print(job_application_instance)
+        
+        job_application_instance = get_results(resume_content=resume_text, preferences=data)
+
+        # print(job_application_instance)
+        
+        return render(request, 'results.html', {'data' : job_application_instance})
     
-    return render(request, 'results.html', {'data' : job_application_instance})
+    return render(request,"waiting.html")
